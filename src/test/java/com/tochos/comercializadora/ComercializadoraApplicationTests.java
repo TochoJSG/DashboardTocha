72d2e2af1da71a51f71110508ddf1be5d1411ded
TochoJSG(JogeSalgado) <jjorgess081@gmail.com>
2024-04-04 18:18:55 -0600

    first Dashboard

---
+++ src/test/java/com/tochos/comercializadora/ComercializadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tochos.comercializadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComercializadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
